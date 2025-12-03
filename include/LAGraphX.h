ddcf2dd9b431ac79a2f0ab5e352bfe2aea9f5b6a
Sterkachov <anymeshnik2.1@gmail.com>
2025-12-03 09:28:14 +0300

    feat: add RSM_reachability algorithm

--- include/LAGraphX.h
+++ include/LAGraphX.h
@@ -854,7 +854,36 @@ int LAGraph_RegularPathQuery    // nodes reachable from the starting by the
     const GrB_Index *S,         // source vertices to start searching paths
     size_t ns,                  // number of source vertices
     char *msg                   // LAGraph output message
-);
+) ;
+
+//****************************************************************************
+LAGRAPHX_PUBLIC
+int LAGraph_RSM_reachability
+(
+    // output:
+    GrB_Vector *reachable, // reachable(i) = true if node i is reachable
+                           // from one of the starting nodes by a path
+                           // satisfying regular constraints
+
+    LAGraph_Graph *R,      // input recursive state machine's
+                           // adjacency matrix decomposition
+    size_t nl,             // total label count, # of matrices graph and
+                           // RSM adjacency matrix decomposition
+    const GrB_Index *QS,   // starting states in RSM
+    size_t nqs,            // number of starting states in RSM
+    const GrB_Index *QF,   // final states in RSM
+    size_t nqf,            // number of final states in RSM
+
+    LAGraph_Graph *R_call, // recursive state machine's call/return nr*nr
+                           // matrix, where (i, j) = 1 if there is transition
+                           // from i state of one automata to j starting 
+                           // state of another automata
+    LAGraph_Graph *G,      // input graph adjacency matrix decomposition
+    const GrB_Index *S,    // source vertices to start searching paths
+    size_t ns,             // number of source vertices
+    char *msg              // LAGraph output message
+) ;
+
 //****************************************************************************
 LAGRAPHX_PUBLIC
 int LAGraph_VertexCentrality_Triangle       // vertex triangle-centrality
